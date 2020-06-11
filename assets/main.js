41d9148741540e4d53b327a7052a36afa02531ce
blongden73 <hello@benlongdendesign.co.uk>
2020-06-11 19:10:34 +0100

    added styles

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,6 @@
+var homepage = document.querySelectorAll('.eh-image_cover');
+var post = document.querySelectorAll('.intro_image');
+
 function elementInViewport(el) {
   var top = el.offsetTop;
   var left = el.offsetLeft;
@@ -33,7 +36,91 @@ function imagesScroller(){
   })
 }
 
+function color(selector, parent){
+  const colorThief = new ColorThief();
+    const imgs = document.querySelectorAll(selector);
+    // Make sure image is finished loading
+    //loop over images
+    for(i = 0; i < imgs.length; i++){
+      //wait for colours to load
+      if (imgs[i].complete) {
+        //colourTheif objec
+        var colours = colorThief.getColor(imgs[i]);
+        console.log('home', homepage);
+        //check if homepage or post
+        if(homepage.length >= 1) {
+          console.log('home', homepage);
+          //get parent
+          var parent = imgs[i].parentNode;
+          //colours in string
+          var colourSring = colours.toString();
+          //set parent background colour
+          parent.style.backgroundColor = 'rgba(' + colourSring + ',1)';
+        } else {
+          console.log('else');
+          //checks the brightness of the colour
+          colourCheck = colours[0] + colours[1] + colours[2];
+          //get parent
+          parent = document.querySelector(parent);
+          //colours to string
+          var colourSring = colours.toString();
+          console.log(colourCheck, 'check');
+          //sets background colour
+          parent.style.backgroundColor = 'rgba(' + colourSring + ',0.8)';
+          //sets dark classs if the colour is dark
+          if(colourCheck <= 270) {
+            parent.classList.add('darkColour');
+          }
+        }
+      } else {
+        imgs[i].addEventListener('load', function() {
+          colorThief.getColor(imgs[i]);
+          console.log('loaded else')
+        });
+      }
+    }
+}
+
+function carousel(){
+  var wrapper = document.querySelector('.eh-post__wrapper');
+  var carousel = document.querySelectorAll('.eh-carousel');
+  var pager = document.querySelectorAll('.eh-image_number');
+  pager[0].classList.add('current');
+  carousel[0].classList.add('display');
+  wrapper.addEventListener('click', function(){
+    console.log('click');
+    var current = document.querySelector('.eh-carousel.display');
+    var next = document.querySelector('.display + .eh-carousel');
+    var pageCurrent = document.querySelector('.eh-image_number.current');
+    var pageNext = document.querySelector('.current + .eh-image_number');
+    current.classList.remove('display');
+    pageCurrent.classList.remove('current');
+    if(next){
+      next.classList.add('display');
+      pageNext.classList.add('current');
+    } else {
+      next = carousel[0];
+      pageNext = pager[0];
+      next.classList.add('display');
+      pageNext.classList.add('current');
+    }
+    console.log(next);
+  });
+}
+
+
 function init(){
-  console.log('init', 'v1.1');
+  console.log('init', 'v2');
   imagesScroller();
+  if(homepage) {
+    setTimeout(function(){
+      color('.eh-image_cover');
+    },1000)
+  }
+  if(post) {
+    setTimeout(function(){
+      color('.intro_image', '.eh-cover');
+    },1000)
+    carousel();
+  }
 } init();
